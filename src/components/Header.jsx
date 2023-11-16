eb24abff91155aeb53332a9ebd1bd958cc7491ca
Inti Silva <inti.t.silva@gmail.com>
2023-11-16 19:15:51 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <h1 className="font-black text-indigo-700 text-5xl text-center md:w-2/3 mx-auto">
+        Veterinary&apos;s {""}
+            <span className="text-black">Patients follow-up</span>
+    </h1>
+  )
+}
+
+export default Header
+
